8a08ea0cc5b1bbec4d0d8fbebbe37002f1c08163
PACRIM1\h144198 <Windy.liu@honeywell.com>
2017-08-30 15:33:04 +0800

    finished cert authority

--- src/main/java/ApplicationStart.java
+++ src/main/java/ApplicationStart.java
@@ -4,6 +4,7 @@ import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.TrustManagerFactory;
 import java.io.IOException;
+import java.net.URL;
 import java.security.*;
 
 import java.io.FileInputStream;
@@ -32,16 +33,21 @@ public class ApplicationStart {
     static Connection connect;
 
     public static void main(String[] args)  {
-        System.out.printf("Start !");
+        System.out.println("Start !");
 
         try
         {
             //sendNatsWithoutSSL();
+            System.out.println(ApplicationStart.class.getResource("client.jks") );
+            URL clientCert = ApplicationStart.class.getResource("client.jks");
+
+            System.out.println(ApplicationStart.class.getResource("client.jks") );
+            URL rootCert = ApplicationStart.class.getResource("troot.jks");
 
             NatsSSLInfo nsi = new NatsSSLInfo();
-            nsi.setKeyStoreLocation("C:\\Workspace\\StudyDemo\\natsClient\\src\\main\\resources\\client.jks");
+            nsi.setKeyStoreLocation(clientCert.getPath());
             nsi.setKeyStorePassPhrase("111111");
-            nsi.setTrustStoreLocation("C:\\Workspace\\StudyDemo\\natsClient\\src\\main\\resources\\tserver.jks");
+            nsi.setTrustStoreLocation(rootCert.getPath());
             nsi.setTrustStorePassPhrase("111111");
             nsi.setNatsUsername("home");
             nsi.setNatsPassword("1qaz2wsx");
@@ -53,20 +59,10 @@ public class ApplicationStart {
             System.out.print(ex);
         }
 
-        System.out.printf("End !");
+        System.out.println("End !");
     }
 
-    private static void sendNatsWithoutSSL() throws IOException {
-        // Connect to default URL ("nats://localhost:4222")
-        Connection nc = Nats.connect("nats://localhost:4222");
-        nc.subscribe("test");
 
-        Message msg = new Message();
-        msg.setSubject("test");
-        String message = "Hello";
-        msg.setData(message.getBytes());
-        nc.publish(msg);
-    }
 
     private static void sendNatsWithSSL(String natsServers,NatsSSLInfo nsi) throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException, IOException {
 
@@ -106,15 +102,6 @@ public class ApplicationStart {
         try{
             connect = natsFactory.createConnection();
 
-//            MessageHandler cb = new NatsMessageHandler();
-//            connect.subscribe("test",cb);
-//
-//            Message msg = new Message();
-//            msg.setSubject("test");
-//            String message = "Hello";
-//            msg.setData(message.getBytes());
-//            connect.publish(msg);
-
             // Simple Async Subscriber
             connect.subscribe("foo", m -> {
                 System.out.printf("Received a message: %s\n", new String(m.getData()));
@@ -130,45 +117,6 @@ public class ApplicationStart {
                 connect.close();
             }
             System.out.print(ex);
-        }
-
-
-
-
-    }
-
-    private static void sendNatsWithSSLOffical(NatsSSLInfo nsi) throws NoSuchAlgorithmException, KeyStoreException, KeyManagementException, IOException, CertificateException, UnrecoverableKeyException {
-        // Set up and load the keystore
-        final KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
-        final char[] keyPassPhrase = nsi.getKeyStorePassPhrase().toCharArray();
-        final KeyStore ks = KeyStore.getInstance(nsi.getKeyStoreType());
-        ks.load(new FileInputStream(nsi.getKeyStoreLocation()), keyPassPhrase);
-        kmf.init(ks, keyPassPhrase);
-
-        // Set up and load the trust store
-        final char[] trustPassPhrase = nsi.getTrustStorePassPhrase().toCharArray();
-        final KeyStore tks = KeyStore.getInstance("JKS");
-        tks.load(new FileInputStream(nsi.getTrustStoreLocation()), trustPassPhrase);
-        final TrustManagerFactory tmf = TrustManagerFactory.getInstance("SunX509");
-        tmf.init(tks);
-
-        // Get and initialize the SSLContext
-        SSLContext c = SSLContext.getInstance(nsi.getProtocol());
-        c.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
-
-        // Create NATS options
-        Options opts = new Options.Builder()
-                .secure()       // Set the secure option, indicating that TLS is required
-                .tlsDebug()     // Set TLS debug, which will produce additional console output
-                .sslContext(c)  // Set the context for this factory
-                .build();
-
-        // Create a new SSL connection
-        try (Connection connection = Nats.connect("nats://115.159.114.116:4222", opts)) {
-            connection.publish("foo", "Hello".getBytes());
-            connection.close();
-        } catch (Exception e) {
-            e.printStackTrace();
         }
     }
 }
